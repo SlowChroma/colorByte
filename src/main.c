ca4d294d60c85fb5509c088872bdf7679f9bc042
SlowChroma <arturo.sukovich@gmail.com>
2024-06-17 12:11:54 -0400

    Release 1.0.0 no known issues.

--- src/main.c
+++ src/main.c
@@ -3,94 +3,148 @@
 
 #define MAX_INPUT_CHARS 8
 
-//TODO: FIX FLASHING COLOR BUG
-
 int main()
 {
-  InitWindow(500, 700, "colorByte");
-  SetTargetFPS(60);
-  
-  Texture2D background = LoadTexture("./src/background.png");
-  
-  char input[MAX_INPUT_CHARS + 1] = "\0";
-  int numCount = 0;
-  bool mouseOnText = false;
-  int framesCounter = 0;
-  Rectangle textBox = {150, 110, 200, 60};
-  
-  while(!WindowShouldClose())
+  bool restart = true;
+  while(restart)
   {
-    if (CheckCollisionPointRec(GetMousePosition(), textBox)) mouseOnText = true;
-    else mouseOnText = false;
-    
-    if (mouseOnText)
-    {
-      SetMouseCursor(MOUSE_CURSOR_IBEAM);
-      
-      int key = GetCharPressed();
+    restart = false;
 
-      while (key > 0)
+    InitWindow(500, 200, "colorByte");
+    SetTargetFPS(60);
+    
+    char input[MAX_INPUT_CHARS + 1] = "\0";
+    int numCount = 0;
+    bool mouseOnText = false;
+    bool mouseOnButton = false;
+    bool buttonPressed = false;
+    int framesCounter = 0;
+    Rectangle textBox = {25, 110, 200, 60};
+    Rectangle button = {275, 110, 200, 60};
+
+    while(!WindowShouldClose())
+    {
+      if (CheckCollisionPointRec(GetMousePosition(), textBox)) mouseOnText = true;
+      else mouseOnText = false;
+      
+      if (CheckCollisionPointRec(GetMousePosition(), button)) mouseOnButton = true;
+      else mouseOnButton = false;
+
+      if (mouseOnText)
       {
-        if(((key == 48) || (key == 49)) && (numCount < MAX_INPUT_CHARS))
+        SetMouseCursor(MOUSE_CURSOR_IBEAM);
+
+        int key = GetCharPressed();
+
+        while (key > 0)
         {
-          input[numCount] = (char)key;
-          input[numCount+1] = '\0'; // Add null terminator at the end of the string.
-          numCount++;
+          if(((key == 48) || (key == 49)) && (numCount < MAX_INPUT_CHARS))
+          {
+            input[numCount] = (char)key;
+            input[numCount+1] = '\0'; // Add null terminator at the end of the string.
+            numCount++;
+          }
+
+          key = GetCharPressed();
         }
 
-        key = GetCharPressed();
+        if (IsKeyPressed(KEY_BACKSPACE))
+        {
+          numCount--;
+          if (numCount < 0) numCount = 0;
+          input[numCount] = '\0';
+        }
       }
+      else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 
-      if (IsKeyPressed(KEY_BACKSPACE))
+      if (mouseOnText) framesCounter++;
+      else framesCounter = 0;
+
+      BeginDrawing();
+
+      ClearBackground(BLACK);
+      DrawText("colorByte v2.0.0", 400, 180, 10, WHITE);
+
+      DrawRectangleRec(textBox, LIGHTGRAY);
+
+      if (mouseOnText) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, RED);
+      else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
+
+      DrawText(input, (int)textBox.x + 5, (int)textBox.y + 8, 40, BLACK);
+
+      if (mouseOnText)
       {
-        numCount--;
-        if (numCount < 0) numCount = 0;
-        input[numCount] = '\0';
+        if (numCount < MAX_INPUT_CHARS)
+        {
+          if (((framesCounter/20)%2) == 0) DrawText("_", (int)textBox.x + 8 + MeasureText(input, 40), (int)textBox.y + 12, 40, BLACK);
+        }
       }
+
+      DrawText("Welcome to colorByte!\n\nHover you mouse over the box and\n\ninput a colorByte.", 10, 10, 20, WHITE);
+
+      DrawRectangleRec(button, LIGHTGRAY);
+
+      if(numCount == 8)
+      {
+        DrawText("DISPLAY\n\n  COLOR", 300, 110, 30, BLACK);
+      }
+
+      if(numCount == 8 && mouseOnButton)
+      {
+        DrawRectangleRec(button, DARKGRAY);
+        DrawText("DISPLAY\n\n  COLOR", 300, 110, 30, BLACK);
+        if (mouseOnButton) DrawRectangleLines((int)button.x, (int)button.y, (int)button.width, (int)button.height, RED);
+        else DrawRectangleLines((int)button.x, (int)button.y, (int)button.width, (int)button.height, DARKGRAY);
+        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+          buttonPressed = true;
+          break;
+        }
+      }
+
+      EndDrawing();
     }
-    else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
-
-    if (mouseOnText) framesCounter++;
-    else framesCounter = 0;
     
-    BeginDrawing();
+    CloseWindow();
     
-    ClearBackground(BLACK);
-    
-    DrawTexture(background, 0, 200, WHITE);
-    DrawText("Welcome to colorByte!\n\nHover you mouse over the box and\n\ninput a colorByte.", 10, 10, 20, WHITE);
-    
-    DrawRectangleRec(textBox, LIGHTGRAY);
-    if (mouseOnText) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, RED);
-    else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
-
-    DrawText(input, (int)textBox.x + 5, (int)textBox.y + 8, 40, MAROON);
-
-    if (mouseOnText)
+    if(numCount == 8 && buttonPressed)
     {
-      if (numCount < MAX_INPUT_CHARS)
-      {
-        if (((framesCounter/20)%2) == 0) DrawText("_", (int)textBox.x + 8 + MeasureText(input, 40), (int)textBox.y + 12, 40, MAROON);
-      }
-    }
+      int rgba[4];
 
-    int rgba[4];
-    
-    if(numCount == 8){
       for(int i = 0; i < 4; i++)
       {
-      rgba[i] = byte_to_rgba(input)[i];        
+        rgba[i] = byte_to_rgba(input)[i];
       }
+
+      Color colorByteColor = {rgba[0], rgba[1], rgba[2], rgba[3]};
+
+      InitWindow(500, 600, "colorByte");
+
+      Texture2D background = LoadTexture("./src/background.png");
+
+      while(!WindowShouldClose())
+      {
+        BeginDrawing();
+
+        ClearBackground(DARKGRAY);
+        DrawTexture(background, 0, 0, WHITE);
+        DrawRectangleLines(0, 500, 500, 100, RED);
+        DrawText("R TO RESTART\n\nESC TO QUIT", 25, 525, 30, BLACK);
+
+        DrawRectangle(0, 0, 500, 500, colorByteColor);
+
+        EndDrawing();
+
+        if(IsKeyPressed(KEY_R))
+        {
+          restart = true;
+          break;
+        }
+      }
+
+      UnloadTexture(background);
+
+      CloseWindow();
     }
-    
-    Color colorByteColor = {rgba[0], rgba[1], rgba[2], rgba[3]};
-    
-    DrawRectangle(0, 200, 500, 500, colorByteColor);
-    
-    EndDrawing();
   }
-  
-  CloseWindow();
-  
-  return 0;
 }
